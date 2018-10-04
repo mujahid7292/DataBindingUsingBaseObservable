5454cdfd63dcc88840a33c8130b61703ec53ee5d
Saifullah Al Mujahid <mujahid7292@gmail.com>
2018-10-04 08:19:05 +0600

    Finish

--- app/src/main/java/com/sand_corporation/www/databindingusingbaseobservable/MainViewModel.java
+++ app/src/main/java/com/sand_corporation/www/databindingusingbaseobservable/MainViewModel.java
@@ -1,25 +1,32 @@
 package com.sand_corporation.www.databindingusingbaseobservable;
 
-public class MainViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class MainViewModel extends BaseObservable {
     public String email, password;
 
     public MainViewModel() {
 
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 
+    @Bindable
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+        notifyPropertyChanged(BR.password);
     }
 }
